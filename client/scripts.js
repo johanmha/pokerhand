3d3036b37031fad72e696eefd1270c9e11281b06
Johan Haugland <johan.haugland@no.abb.com>
2022-02-19 14:38:08 +0100

    Add script to get html and css friendly suite/value

--- client/scripts.js
+++ client/scripts.js
@@ -1,24 +1,65 @@
 
 //Handler
-document.querySelector('#redraw').addEventListener('click', function(e){
-        e.preventDefault();
-        console.log('sniksnæk');
-        dealHand();
+document.querySelector('#redraw').addEventListener('click', function (e) {
+    e.preventDefault();
+    console.log('sniksnæk');
+    dealHand();
 });
 
 //Helpers
 function dealHand() {
-    cardBuilder('8', 'h')
+    cardBuilder('8', 'd')
 }
 
 function cardBuilder(value, suite) {
-    let htmlSuite = ''
     let htmlValue = ''
+    let htmlSuite = ''
+    let cssSuite = ''
+
 
     switch (value) {
-
+        case 't':
+            htmlValue = '10';
+            break;
+        case 'j':
+            htmlValue = 'J';
+            break;
+        case 'q':
+            htmlValue = 'Q';
+            break;
+        case 'k':
+            htmlValue = 'K';
+            break;
+        case 'a':
+            htmlValue = 'A';
+            break;
+        default:
+            htmlValue = value;
     }
-    
+
+    switch (suite) {
+        case 'h':
+            htmlSuite = '&heartsuit;';
+            cssSuite = 'hearts';
+            break;
+        case 'd':
+            htmlSuite = '&diamondsuit;';
+            cssSuite = 'diamonds';
+            break;
+        case 'k':
+            htmlSuite = '&clubsuit;';
+            cssSuite = 'clubs';
+            break;
+        case 's':
+            htmlSuite = '&spadesuit;';
+            cssSuite = 'spades';
+            break;
+        default:
+            //Display suite input if there is error input
+            htmlSuite = suite;
+            cssSuite = 'spades';
+    }
+
     let cards = document.querySelector('.cards');
     let card = document.createElement('span');
     let cardSection = document.createElement('div');
@@ -31,8 +72,8 @@ function cardBuilder(value, suite) {
     cardTop.classList.add('card-top');
 
     cardTop.innerHTML = `
-        <i class="clubs">&clubsuit;</i>
-        <i class="value">9</i>
+        <i class="${cssSuite}">${htmlSuite}</i>
+        <i class="value">${htmlValue}</i>
     `;
     card.appendChild(cardSection).appendChild(cardTopContainer).appendChild(cardTop);
     cards.appendChild(card)
